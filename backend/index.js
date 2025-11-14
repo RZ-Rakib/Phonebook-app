eff161e66e8514ce1f2ff1861ea6f88d6106500c
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-11-14 13:35:58 +0200

    3.14: Phonebook database, step 2

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const winston = require('./loggers/logger');
 const morgan = require('morgan');
 const Person = require('./models/person')
@@ -25,107 +26,99 @@ app.get('/api/persons', (req, res) => {
   })
 });
 
-// app.get('/info', (req, res) => {
-//   try {
-//     const countPersons = contacts.length;
-//     const date = new Date();
+app.get('/info', (req, res) => {
+  try {
+    const countPersons = contacts.length;
+    const date = new Date();
     
-//     res.send(`
-//       <p>Phonebook has info for ${countPersons} people</p>
-//       <p>${date}</P>
-//       `);
-//       winston.info(`'GET /info' fetched info sucessfully`);
-//     } catch (error) {
-//       winston.error(
-//         `'GET /info' Failed to fetch data from server, ${error.message}`
-//       );
-//       res.status(500).end();
-//     }
-//   });
+    res.send(`
+      <p>Phonebook has info for ${countPersons} people</p>
+      <p>${date}</P>
+      `);
+      winston.info(`'GET /info' fetched info sucessfully`);
+    } catch (error) {
+      winston.error(
+        `'GET /info' Failed to fetch data from server, ${error.message}`
+      );
+      res.status(500).end();
+    }
+    mongoose.connection.close()
+  });
   
-//   app.get('/api/persons/:id', (req, res) => {
-//     try {
-//       const id = req.params.id;
-//       if (!id) {
-//         winston.warn(`'GET /api/persons/:id' - ID is missing`);
-//         return res.status(400).json({ error: 'invalid id' });
-//       }
-//       const person = contacts.find((n) => n.id === id);
-//       if (person) {
-//         res.json(person);
-//         winston.info(`'GET api/persons/:id' - person with ID ${id} found`);
-//       } else {
-//         winston.warn(`'GET api/person/:id' -  id ${id} is invalid`);
-//         res.status(404).end();
-//       }
-//     } catch (error) {
-//       winston.error(
-//         `'GET api/persons/:ID' - Failed to fetch data from server, ${error.message}`
-//       );
-//       res.status(500).end();
-//     }
-//   });
+  app.get('/api/persons/:id', (req, res) => {
+    try {
+      const id = req.params.id;
+      if (!id) {
+        winston.warn(`'GET /api/persons/:id' - ID is missing`);
+        return res.status(400).json({ error: 'invalid id' });
+      }
+      const person = contacts.find((n) => n.id === id);
+      if (person) {
+        res.json(person);
+        winston.info(`'GET api/persons/:id' - person with ID ${id} found`);
+      } else {
+        winston.warn(`'GET api/person/:id' -  id ${id} is invalid`);
+        res.status(404).end();
+      }
+    } catch (error) {
+      winston.error(
+        `'GET api/persons/:ID' - Failed to fetch data from server, ${error.message}`
+      );
+      res.status(500).end();
+    }
+  });
   
-//   app.delete('/api/persons/:id', (req, res) => {
-//     try {
-//       const id = req.params.id;
-//       if (!id) {
-//         winston.warn(`'DELETE' api/persons/:id - ID is missing`);
-//         return res.status(400).json({ error: 'ID missing' });
-//       }
+  app.delete('/api/persons/:id', (req, res) => {
+    try {
+      const id = req.params.id;
+      if (!id) {
+        winston.warn(`'DELETE' api/persons/:id - ID is missing`);
+        return res.status(400).json({ error: 'ID missing' });
+      }
       
-//       contacts = contacts.filter((n) => n.id !== id);
-//       res.status(204).end();
-//       winston.info(`'DELETE' api/persons/:id - person with ID ${id} deleted`);
-//     } catch (error) {
-//       res.status(500).end();
-//       winston.error(
-//         `'DELETE' api/persons/:id - Error deleting person with ID ${id} from server', ${error.message}`
-//       );
-//     }
-//   });
+      contacts = contacts.filter((n) => n.id !== id);
+      res.status(204).end();
+      winston.info(`'DELETE' api/persons/:id - person with ID ${id} deleted`);
+    } catch (error) {
+      res.status(500).end();
+      winston.error(
+        `'DELETE' api/persons/:id - Error deleting person with ID ${id} from server', ${error.message}`
+      );
+    }
+  });
   
-//   const generatedID = (min, max) => {
-//     const ceiledMin = Math.ceil(min);
-//     const flooredMax = Math.floor(max);
-//     return Math.floor(Math.random() * (flooredMax - ceiledMin) + ceiledMin);
-//   };
-//   app.post('/api/persons', (req, res) => {
-//     try {
-//       const { name, number } = req.body;
-//       if (!name || !number) {
-//         winston.warn(`'POST api/persons/' - Missing required fields`);
-//         return res.status(400).json({ error: 'Missing name or number' });
-//       }
+  app.post('/api/persons', (req, res) => {
+      const { name, number } = req.body;
+
+      if (!name || !number) {
+        winston.warn(`'POST api/persons/' - Missing required fields`);
+        return res.status(400).json({ error: 'Missing name or number' });
+      }
       
-//       const trimmer = (value) => {
-//         return typeof value === 'string' ? value.trim().toLowerCase() : '';
-//       };
-      
-//       const existingName = contacts.find(
-//         (p) => trimmer(p.name) === trimmer(name)
-//       );
-//       if (existingName) {
-//         winston.warn(`'POST api/persons/' - name:${name} must be unique`);
-//         return res.status(400).json({ error: 'name must be unique' });
-//       }
-      
-//       const newObject = {
-//         id: generatedID(10000, 100000),
-//         name: name,
-//         number: number,
-//       };
-//       contacts = contacts.concat(newObject);
-      
-//       res.status(201).json(newObject);
-//       winston.info(`'POST api/persons' - name:${name} is created sucessfully`);
-//     } catch (error) {
-//       res.status(500).end();
-//       winston.error(
-//         `'POST' api/persons - Failed to add name:${name} to server, ${error.message}`
-//       );
-//     }
-//   });
+      Person.findOne({name: name.trim()})
+        .then(existingPerson => {
+          if (existingPerson) {
+            winston.warn(`'POST api/persons/' - name:${name} must be unique`);
+            res.status(400).json({ error: 'name must be unique' });
+            return null
+          }
+          const person = new Person({
+            name: name.trim(),
+            number: number.trim(),
+          });
+          return person.save()
+        })
+        .then(savedPerson => {
+          if(savedPerson){
+            winston.info(`'POST api/persons' - name:${name} is created sucessfully`);
+            res.status(201).json(savedPerson);
+          }
+        })
+        .catch(error => {
+          winston.error(`'POST' api/persons - Failed to add name:${name} to server, ${error.message}`)
+          res.status(500).end();
+        })
+  });
 
   app.use(express.static('dist'));
   
